d858301966e1759f57435079e5b4c856bd152e39
Muart-C <charles.muathe@strathmore.edu>
2019-02-18 15:49:10 +0300

    [feature #164044036] add can edit comment

--- app/users.py
+++ app/users.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from comments import Comment
+from app.comments import Comment
 
 """manage users"""
 class User():
@@ -7,7 +7,6 @@ class User():
         self.name = name
         self._last_seen = None
         self._is_logged_in = False
-        self._lobby = Comment()
         self._class_received = self.__class__.__name__
 
     def get_name(self):
@@ -25,18 +24,26 @@ class User():
     def login(self, _online_status):
         self._is_logged_in = True
         return self._is_logged_in
-        
+
     def log_out(self):
         self._is_logged_in = False
         self.last_logged_in_at()
         return self._is_logged_in
   
-  
+    def can_edit(self, comment,id):
+        if self.name == comment['author']:
+            Comment().get_list()[id]['message'] = comment['message']
+            return 'Edited'
 class Moderator(User):
 
     def delete_comment(self, commentobj, id):
         return commentobj.delete_comment(id)
 
+
 class Admin(Moderator):
     def __init__(self, name):
         super().__init__(name)
+
+    def can_edit(self, comment,id):
+        Comment().get_list()[id]['message'] = comment['message']
+        return 'Edited'
